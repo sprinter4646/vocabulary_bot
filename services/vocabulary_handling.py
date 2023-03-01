6b61a353bc197e959890200554f36d8385b6ad3b
oleg <shishatskiy@mail.ru>
2023-03-01 12:29:11 +0700

    user_handlers.py подключен

--- services/vocabulary_handling.py
+++ services/vocabulary_handling.py
@@ -1,21 +1,19 @@
-VOCABULARY_PATH = '../vocabulary/2000full.txt'
+VOCABULARY_PATH = '/home/oleg/REPOS/Vocabulari_Bot/vocabulary/2000full.txt'
 
 
-# Функция, формирующая словарь словари
-'''def prepare_vocabularies(path: str) -> type:
+'''# Функция, формирующая словарь словари
+def prepare_vocabularies(path: str) -> dict:
     with open(path, 'r', encoding='utf-8') as file:
         eng_ru: dict[str, str] = {(row[:row.index('\t')]): row[row.index('\t') + 1:row.index('\n')] for row in file}
         ru_eng: dict[str, str] = dict(zip(eng_ru.values(), eng_ru.keys()))
         print(eng_ru)
         print(ru_eng)
-        return eng_ru, ru_eng'''
+        return ru_eng
 
 
-# Вызов функции prepare_book для подготовки книги из текстового файла
-# prepare_vocabularies(VOCABULARY_PATH)
+# Вызов функции prepare_vocabularies для подготовки словарей из текстового файла
+prepare_vocabularies(VOCABULARY_PATH)'''
 
 with open(VOCABULARY_PATH, 'r', encoding='utf-8') as file:
-    ru_eng: dict[str, str] = {(row[:row.index('\t')]): row[row.index('\t') + 1:row.index('\n')] for row in file}
-    # ru_eng: dict[str, str] = dict(zip(ru_eng.values(), ru_eng.keys()))
-    print(ru_eng)
-    # print(eng_ru)
+    eng_ru: dict[str, str] = {(row[:row.index('\t')]): row[row.index('\t') + 1:row.index('\n')] for row in file}
+    ru_eng: dict[str, str] = dict(zip(eng_ru.values(), eng_ru.keys()))
