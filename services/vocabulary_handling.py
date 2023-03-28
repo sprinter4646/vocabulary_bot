ca9b16f714cb33fcfc9eb70745674e30cf866cf8
oleg <shishatskiy@mail.ru>
2023-03-28 11:53:35 +0700

    В README.md добавляем блок-схему

--- services/vocabulary_handling.py
+++ services/vocabulary_handling.py
@@ -1,8 +1,6 @@
 # модуль для подготовки словарей из текстового файла
-VOCABULARY_PATH = '/home/oleg/REPOS/Vocabulari_Bot/vocabulary/2000full.txt'
-
+VOCABULARY_PATH = r'/home/oleg/Yandex.Disk/PYTHON_ubuntu/REPOS/vocabulary_bot/vocabulary/2000full.txt'
 
 with open(VOCABULARY_PATH, 'r', encoding='utf-8') as file:
     eng_ru: dict[str, str] = {(row[:row.index('\t')]): row[row.index('\t') + 1:row.index('\n')] for row in file}
     ru_eng: dict[str, str] = dict(zip(eng_ru.values(), eng_ru.keys()))
-
